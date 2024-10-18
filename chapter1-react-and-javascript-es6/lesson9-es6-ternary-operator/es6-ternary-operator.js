c93b22f530be2e0617d53971c9dfd4683d3f1d37
locnht-it <locnht.it@gmail.com>
2024-10-19 00:24:54 +0700

    Lesson 9: ES6 Ternary Operator

---
+++ chapter1-react-and-javascript-es6/lesson9-es6-ternary-operator/es6-ternary-operator.js
@@ -0,0 +1,14 @@
+console.log(`Lesson9 - Ternary Operator`);
+
+const x = 1;
+let y = ``;
+// if (x > 5) {
+//   console.log(`Greater than 5`);
+// } else {
+//   console.log(`Less or equal 5`);
+// }
+
+// x > 5 ? console.log(`Greater than 5`) : console.log(`Less or equal 5`);
+
+y = x > 5 ? "Greater than 5" : "Less or equal 5";
+console.log(y);
